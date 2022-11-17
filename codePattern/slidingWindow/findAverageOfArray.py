69dcb7f25aaa52622bc1d1040a4cfadda91c9470
Zumh <17211423+Zumh@users.noreply.github.com>
2022-11-16 20:20:51 -0500

    optimize solution for finding average of subarray

--- codePattern/slidingWindow/findAverageOfArray.py
+++ codePattern/slidingWindow/findAverageOfArray.py
@@ -29,4 +29,38 @@ def find_average_array(nums:list[int], k:int)->list[int]:
         outerIndex += 1 
     return averages
 nums = [1,3,2,6,-1,4,1,8,2]
-print(find_average_array(nums,5)) 
+print(find_average_array(nums,5))
+
+# Window sliding average sub array
+def find_average_array_slide(nums:list, k:int)->list[int]:
+    """
+        Sliding window allow us to optimize the algorithm effeciency from O(N^2) to O(N)
+    """
+    averages = []
+
+    totalSum = 0
+    startWindow = 0
+    endWindow = k 
+    for current_index, num in enumerate(nums):
+        
+        # get a new total sum within the k size
+        totalSum += num 
+
+        endWindow = current_index
+
+        if endWindow+1 >= k:
+            # calculate the average of subarray 
+            averages.append(totalSum/k)
+
+            # update a new total by removing the first value from the total sum
+            totalSum = totalSum - nums[startWindow]
+
+            # move the endWindow forward 
+            endWindow += 1
+            # move the startWindow forward
+            startWindow += 1
+         
+    return averages 
+nums = [1,3,2,6,-1,4,1,8,2]
+print(find_average_array_slide(nums,5))
+
