dcd91e0b3e7d4c0323c02665bf511442253287ac
Zumh <17211423+Zumh@users.noreply.github.com>
2022-11-21 12:16:45 -0500

    fixed merge two list by sorting and insert them in list1 then extends the left over elements to list1

---
+++ acePython/1.ds/lists/1_merge_lists.py
@@ -0,0 +1,26 @@
+
+def merge_lists(list1:list[int], list2:list[int])->list[int]:
+
+    # merge two sorted lists assuming both have same length
+    # we going to merge them in the first list
+    result = []
+    left = 0
+    right = 0
+    
+    while left < len(list1) and right < len(list2):
+
+        if list1[left] > list2[right]:
+            list1.insert(left, list2[right])
+            left += 1
+            right += 1
+        else:
+            left += 1
+    # get the last remain numbers
+    if right < len(list2):
+        list1.extend(list2[right:])
+    return list1
+
+num_1 = [1,3,4,5]
+num_2 = [2,6,7,8]
+
+print(merge_lists(num_1, num_2))
